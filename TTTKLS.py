fa6047e19a3338f0150638beb062a3c066e8388c
Kenandrae Wheatt <kenandraewheatt@Kenandraes-Mac-mini.local>
2021-12-08 19:20:09 -0800

    up to line 105

--- TTTKLS.py
+++ TTTKLS.py
@@ -12,12 +12,12 @@
 # * **token** = *'X' or 'O'*
 
 # The Game class has the following properties:
-# * **board** = *your representation of the board*
+
 
 # You can represent the board however you like, such as a 2D list, tuples, or dictionary.
 
 # The Game class has the following methods:
-# * `__repr__()` Returns a pretty string representation of the game board
+# * 
 # ```py
 # >>> print(board)
 # X| | 
@@ -25,7 +25,7 @@
 #  | | 
 # ```
 
-# * `move(x, y, player)` Place a player's token character string at a given coordinate (top-left is 0, 0), x is horizontal position, y is vertical position.
+
 
 # ```py
 # >>> board.move(2, 1, player_1)
@@ -34,7 +34,7 @@
 #  | | 
 # ```
 
-# * `calc_winner()` What token character string has won or `None` if no one has.
+
 
 # ```py
 # X| | 
@@ -54,7 +54,7 @@
 # True
 # ```
 
-# * `is_game_over()` Returns true if the game board is full or a player has won.
+
 
 # ```py
 # X|O|X
@@ -69,3 +69,51 @@
 # >>> board.is_game_over()
 # False
 # ```
+"""
+Assumptions =
+1. 2 live players
+2.Player Class
+3.Game Class
+
+print("Let's Play Tic Tac Toe!")
+
+class Player:
+    def __init__(self, name, token):
+        self.name =  name
+        self.token = token 'x'  || 'o'
+
+class Game:
+    def __init__(self,boardstuff):
+        self.boardstuff = boardstuff
+        
+
+user_1 = Player
+user_2 = Player 
+"""
+# * **board** = *your representation of the board*
+#`__repr__()` Returns a pretty string representation of the game board
+# * `is_game_over()` Returns true if the game board is full or a player has won.
+# * `calc_winner()` What token character string has won or `None` if no one has.
+# * `move(x, y, player)` Place a player's token character string at a given coordinate 
+# ------(top-left is 0, 0), x is horizontal position, y is vertical position.-------
+# Three lists 
+# row1
+# 11,21,31
+# row2
+# 21,22,23
+# row3
+# 31,32,33
+
+# [|||]
+# [|||]
+# [|||]
+
+class Test:
+    def __init__(self, board):
+        self.board = board
+    
+    def __repr__(self):
+        return "%s" % (self.board)
+#x = Test(["x", "|", "o", "|", "x"])
+x = Test(["xox"])
+print(x)
